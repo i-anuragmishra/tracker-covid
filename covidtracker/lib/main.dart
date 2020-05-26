c498c01f6da326a645522cdb1d12db0fcb6294f8
anurag <anuragmishra@ANURAGs-MacBook-Air.local>
2020-05-26 14:25:05 +0530

    Added total cases text

--- covidtracker/lib/main.dart
+++ covidtracker/lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-          body: Text(
-            "Total cases",
-            textAlign: TextAlign.center,
+          body: Align(
+            alignment: Alignment.topCenter,
+            child: Text(
+              "Total cases",
+              textScaleFactor: 2.4,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.normal,
+                  color: Colors.pink[500]),
+            ),
           ),
           backgroundColor: Colors.white,
           appBar: AppBar(
